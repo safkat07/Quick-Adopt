e804b6d9caa79805cd4ea4350c1c55db8ae87031
safkat07 <safkatsadli@gmail.com>
2023-12-05 22:23:49 +0600

    navbar animated hover added

--- src/Layout/AllPages/Header/Navbar/Navbar.jsx
+++ src/Layout/AllPages/Header/Navbar/Navbar.jsx
@@ -5,27 +5,27 @@ const Navbar = () => {
     return (
         <div>
             <div className=' bg-[#EBF1EE]'>
-                <div className='flex  shadow-2xl px-5  h-20 lg:flex-row flex-col justify-between   items-center'>
+                <div className='flex  shadow-2xl px-5  h-28 lg:flex-row flex-col justify-between   items-center'>
                     <div className='flex  items-center'>
                         <a className='no-underline text-black' href="#">
-                            <span className='text-3xl font-bold font-lobster tracking-[.4rem]'>QuickAdopt</span>
+                            <span className='text-4xl font-bold font-lobster tracking-[.4rem]'>QuickAdopt</span>
                         </a>
                     </div>
                     <div>
                         <ul className='list-none lg:flex-row flex-col text-center  font-maven text-xl  flex gap-6'>
-                            <li><NavLink className={"no-underline text-black"}>
+                            <li><NavLink className={"no-underline hover:bg-[#327451] rounded-md py-1 hover:text-white hover:px-2 transition-all duration-700 text-black"}>
                                 Home
                             </NavLink></li>
-                            <li><NavLink className={"no-underline text-black"}>
+                            <li><NavLink className={"no-underline hover:bg-[#327451] rounded-md py-1 hover:text-white hover:px-2 transition-all duration-700 text-black"}>
                                 Find A Puppy
                             </NavLink></li>
-                            <li><NavLink className={"no-underline text-black"}>
+                            <li><NavLink className={"no-underline hover:bg-[#327451] rounded-md py-1 hover:text-white hover:px-2 transition-all duration-700 text-black"}>
                                 About us
                             </NavLink></li>
-                            <li><NavLink className={"no-underline text-black"}>
+                            <li><NavLink className={"no-underline hover:bg-[#327451] rounded-md py-1 hover:text-white hover:px-2 transition-all duration-700 text-black"}>
                                 Stories
                             </NavLink></li>
-                            <li><NavLink className={"no-underline text-black"}>
+                            <li><NavLink className={"no-underline hover:bg-[#327451] rounded-md py-1 hover:text-white hover:px-2 transition-all duration-700 text-black"}>
                                 Login
                             </NavLink></li>
 
