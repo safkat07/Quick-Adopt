0533189ec10a6433a3c5641c94acadd80960cc32
safkat07 <safkatsadli@gmail.com>
2023-12-10 22:32:30 +0600

    added preloader in the app

---
+++ src/Components/UseableComponents/Loader/PreLoader.jsx
@@ -0,0 +1,12 @@
+
+import Goo from 'gooey-react'
+import LoadingSpinner from './LoadingSpinner';
+const PreLoader = ({PreLoader}) => {
+    return (
+        <Goo>
+            <LoadingSpinner></LoadingSpinner>
+        </Goo>
+    );
+};
+
+export default PreLoader;
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {Ripple} from 'react-preloaders';
 import {
   RouterProvider, ScrollRestoration,
 } from "react-router-dom";
@@ -7,11 +8,13 @@ import './index.css'
 import router from './Router/Router.jsx';
 import AuthProvider from './Components/Providers/AuthProvider/AuthProvider.jsx';
 import { Toaster } from 'react-hot-toast';
+import PreLoader from './Components/UseableComponents/Loader/PreLoader.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={router} />
+      <Ripple background="#303938" color="#fff"  animation="slide" />
     </AuthProvider>
     <Toaster />
   </React.StrictMode>,
