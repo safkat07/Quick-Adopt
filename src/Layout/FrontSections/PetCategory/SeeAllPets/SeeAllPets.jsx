77da31463e31aa4f6ea363c39e75a43c334172c7
safkat07 <safkatsadli@gmail.com>
2024-01-16 20:54:09 +0600

    see all pets small and medium device responsive fixed

--- src/Layout/FrontSections/PetCategory/SeeAllPets/SeeAllPets.jsx
+++ src/Layout/FrontSections/PetCategory/SeeAllPets/SeeAllPets.jsx
@@ -10,14 +10,14 @@ const SeeAllPets = () => {
     const [categories, isLoading] = UseCategory()
     return (
         <MainContainer>
-            <h2 className="text-5xl font-maven underline font-medium text-center">Collection Of Our All Pets</h2>
+            <h2 className="md:text-5xl text-2xl font-maven underline font-medium text-center">Collection Of Our All Pets</h2>
             <div>
                 {
                     isLoading ? <div className="flex justify-center h-[50vh] items-center">
                         <LoadingSpinner></LoadingSpinner>
                     </div>
                         :
-                        <div className="grid grid-cols-4 justify-center items-center  pt-16 space-y-10">
+                        <div className="xl:grid flex flex-row flex-wrap gap-8 xl:grid-cols-4 justify-center items-center  pt-16  md:space-y-10">
                             {
                                 categories.map(category => <PetCategoryCard key={category._id} {...category} ></PetCategoryCard>)
                             }
